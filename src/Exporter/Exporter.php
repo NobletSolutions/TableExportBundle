a731c2547e493be91ed48a2b0d050e9f180596b7
Nathanael d. Noblet <nathanael@noblet.ca>
2020-02-28 09:28:58 -0700

    New release with new exporter

---
+++ src/Exporter/Exporter.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace NS\TableExportBundle\Exporter;
+
+use PhpOffice\PhpSpreadsheet\Reader\Html;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Twig\Environment;
+
+class Exporter
+{
+    /** @var Environment */
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function export(string $baseFileName, Sheet $sheet): StreamedResponse
+    {
+        $html = $this->twig->render($sheet->getTemplate(), $sheet->getParameters());
+        $headers = [
+            'Content-type' => 'text/vnd.ms-excel; charset=utf-8',
+            'Pragma' => 'public',
+            'Cache-Control' => 'maxage=1',
+            'Content-Disposition' => sprintf('attachment;filename=%s-%s.xls', $baseFileName, date('Y_m_d_H_i_s')),
+        ];
+
+        return new StreamedResponse(static function () use ($html, $sheet) {
+            $reader = new Html();
+            $spreadsheet = $reader->loadFromString($html);
+            if ($sheet->getSafeTitle()) {
+                $spreadsheet->getActiveSheet()->setTitle($sheet->getSafeTitle());
+            }
+
+            $writer = new Xlsx($spreadsheet);
+            $writer->save('php://output');
+        }, Response::HTTP_OK, $headers);
+    }
+
+    /**
+     * @param Sheet[]|array $sheets
+     * @param string $baseFileName
+     * @param array $headers
+     * @return StreamedResponse
+     *
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     * @throws \PhpOffice\PhpSpreadsheet\Reader\Exception
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function multiSheetExport(array $sheets, string $baseFileName, array $headers = []): StreamedResponse
+    {
+        $reader = new Html();
+        $spreadsheet = null;
+        $x = 0;
+
+        foreach ($sheets as $sheet) {
+            if ($spreadsheet) {
+                $x++;
+                $reader->setSheetIndex($x);
+            }
+
+            $spreadsheet = $reader->loadFromString($this->twig->render($sheet->getTemplate(), $sheet->getParameters()), $spreadsheet);
+            if ($sheet->getSafeTitle()) {
+                $spreadsheet->getActiveSheet()->setTitle($sheet->getSafeTitle());
+            }
+        }
+
+        $spreadsheet->setActiveSheetIndex(0);
+
+        $headers = array_merge([
+            'Content-type' => 'text/vnd.ms-excel; charset=utf-8',
+            'Pragma' => 'public',
+            'Cache-Control' => 'maxage=1',
+            'Content-Disposition' => sprintf('attachment;filename=%s-%s.xls', $baseFileName, date('Y_m_d_H_i_s')),
+        ], $headers);
+
+        return new StreamedResponse(static function () use ($spreadsheet) {
+            $writer = new Xlsx($spreadsheet);
+            $writer->save('php://output');
+        }, Response::HTTP_OK, $headers);
+    }
+}
---
+++ src/Exporter/Sheet.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace NS\TableExportBundle\Exporter;
+
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
+class Sheet
+{
+    /** @var string|null */
+    private $title;
+
+    /** @var string */
+    private $template;
+
+    /** @var array|null */
+    private $parameters;
+
+    public function __construct(string $template, ?array $parameters = null)
+    {
+        $this->template = $template;
+        $this->parameters = $parameters;
+    }
+
+    public function setTitle(?string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    /** @var string|null */
+    private $safeTitle;
+
+    public function getSafeTitle(): ?string
+    {
+        if (!$this->title) {
+            return null;
+        }
+
+        if ($this->safeTitle) {
+            return $this->safeTitle;
+        }
+
+        $name = str_replace(Worksheet::getInvalidCharacters(), ' ', $this->title);
+        $this->safeTitle = strlen($name) >= 31 ? substr($name, 0, 25) . '...' : $this->title;
+
+        return $this->safeTitle;
+    }
+
+    public function getTemplate(): string
+    {
+        return $this->template;
+    }
+
+    public function getParameters(): ?array
+    {
+        return $this->parameters;
+    }
+}
